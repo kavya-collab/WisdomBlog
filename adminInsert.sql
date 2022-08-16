75b7bc263e8264ccf89f40acf921c305079b36f0
kavya-collab <kavyach23@gmail.com>
2022-08-16 13:35:29 -0400

    commit

---
+++ adminInsert.sql
@@ -0,0 +1,2 @@
+insert into users values('admin', 'users', 'wisdomAdmin', '3333333', '$2a$11$kaLzcoD99hz2evkunjA22.n5ROx7t1f2xKHWT09/8kVPAL1eHB//2',
+'admin@gmail.com', '9999999999', 'test address', '142369', 1)
