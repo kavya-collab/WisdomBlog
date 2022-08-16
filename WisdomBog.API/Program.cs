1152973af5d8fb03bb75d0e66f7102e18da859ee
kavya-collab <kavyach23@gmail.com>
2022-08-16 13:52:53 -0400

    commit

---
+++ WisdomBog.API/Program.cs
@@ -0,0 +1,44 @@
+﻿using WisdomBlog.API.Authorization;
+using WisdomBlog.API.Services;
+using WisdomBlog.Models.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WisdomBlogDbContext>();
+
+
+// configure strongly typed settings object
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
+
+// configure DI for application services
+builder.Services.AddScoped<IJwtUtils, JwtUtils>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IItemsService, ItemsService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// custom jwt auth middleware
+app.UseMiddleware<JwtMiddleware>();
+
+app.MapControllers();
+
+app.Run();
